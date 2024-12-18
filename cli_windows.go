406fa5dafa73f33b92cb25fb3e2a1889e27be370
David Vogel <Dadido3@aol.com>
2024-12-18 20:31:28 +0100

    Rename typst_*.go into cli_*.go

---
+++ cli_windows.go
@@ -0,0 +1,10 @@
+//go:build windows
+
+package typst
+
+// The path to the typst executable.
+// We assume the executable is in the current working directory.
+//var ExecutablePath = "." + string(filepath.Separator) + filepath.Join("typst.exe")
+
+// The path to the typst executable.
+var ExecutablePath = "typst.exe"
--- typst_windows.go
+++
@@ -1,9 +0,0 @@
-//go:build windows
-
-package typst
-
-import "path/filepath"
-
-// The path to the typst executable.
-// We assume the executable is in the current working directory.
-var ExecutablePath = "." + string(filepath.Separator) + filepath.Join("typst.exe")
