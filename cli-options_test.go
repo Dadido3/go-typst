473563756dbb3ba0888bc3d98cdcff393e702c19
David Vogel <Dadido3@aol.com>
2025-11-03 19:31:11 +0100

    Fix linter warning about yoda conditions

--- cli-options_test.go
+++ cli-options_test.go
@@ -15,10 +15,10 @@ func TestCliOptions(t *testing.T) {
 	if len(args) != 2 {
 		t.Errorf("wrong number of arguments, expected 2, got %d", len(args))
 	}
-	if "--font-path" != args[0] {
+	if args[0] != "--font-path" {
 		t.Error("wrong font path option, expected --font-path, got", args[0])
 	}
-	if "somepath/to/somewhere"+string(os.PathListSeparator)+"another/to/somewhere" != args[1] {
+	if args[1] != "somepath/to/somewhere"+string(os.PathListSeparator)+"another/to/somewhere" {
 		t.Error("wrong font path option, expected my two paths concatenated, got", args[1])
 	}
 }
