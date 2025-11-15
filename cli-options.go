e9bd6a09281fcd7ba92771613afee01112e8b854
David Vogel <Dadido3@aol.com>
2025-11-15 20:40:21 +0100

    Add CLIOptions as an alias type of Options

---
+++ cli-options.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 David Vogel
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package typst
+
+// CLIOptions contains all parameters that can be passed to a Typst CLI.
+//
+// Deprecated: Use typst.Options instead.
+type CLIOptions = Options
